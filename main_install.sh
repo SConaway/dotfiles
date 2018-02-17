8f35588a7565a30cf16915bae76655a7c7aac092
Steven Conaway <steven@Stevens-MBP.router>
2018-02-17 10:19:02 -0800

    Actually Do Something

--- main_install.sh
+++ main_install.sh
@@ -1,54 +1,54 @@
 #! /bin/bash
-caffeinate & # prevent computer from going to sleep
+caffeinate & prevent computer from going to sleep
 
 clear
 echo $on_ci
-if [[ $# -eq 1 ]] ; then
+if [[ $-eq 1 ]] ; then
 #if [[ $on_ci -eq "yes" ]]; then
   echo "Running on CI"
   on_ci=true
 
-  # initial_setup
+  initial_setup
   #
-  # install_python
-  # install_ruby
-  # install_node
-  # install_zsh
+  install_python
+  install_ruby
+  install_node
+  install_zsh
   #
   case $1 in
     '0')
     echo "CI Part 0"
-    # install_brew_apps
+    install_brew_apps
     ;;
     '1')
     echo "CI Part 1"
-    # brew install duti
-    # install_cask_apps_part_1
-    # set_cask_default_apps
-    # install_atom_packages
+    brew install duti
+    install_cask_apps_part_1
+    set_cask_default_apps
+    install_atom_packages
     ;;
 
     '2')
     echo "CI Part 2"
-    # install_cask_apps_part_2
+    install_cask_apps_part_2
     ;;
 
     '3')
     echo "CI Part 3"
-    # install_tinyscripts
-    # copy_commands
-    # link_airport
+    install_tinyscripts
+    copy_commands
+    link_airport
     ;;
 
     '4')
     echo "CI Part 4"
-    # lower_startup_chime
-    # os_customize
-    # configure_dock
-    # cleanup_brew
-    # cleanup_error_log
-    # killall caffeinate # computer can go back to sleep
-    # final_message
+    lower_startup_chime
+    os_customize
+    configure_dock
+    cleanup_brew
+    cleanup_error_log
+    killall caffeinate computer can go back to sleep
+    final_message
     ;;
 
     '5')
@@ -64,44 +64,44 @@ if [[ $# -eq 1 ]] ; then
 else
   on_ci=false
 
-  # initial_setup
-  # ask_details
-  # sync_icloud
-  # update_system
+  initial_setup
+  ask_details
+  sync_icloud
+  update_system
   #
-  # install_brew
-  # install_python
-  # install_ruby
-  # install_node
-  # install_zsh
+  install_brew
+  install_python
+  install_ruby
+  install_node
+  install_zsh
   #
-  # install_brew_apps
-  # install_cask_apps_part_1
-  # install_cask_apps_part_2
-  # install_tinyscripts
-  # install_mas_apps
-  # install_oh_my_zsh
+  install_brew_apps
+  install_cask_apps_part_1
+  install_cask_apps_part_2
+  install_tinyscripts
+  install_mas_apps
+  install_oh_my_zsh
   #
-  # set_brew_default_apps
-  # set_cask_default_apps
-  # configure_zsh
-  # install_atom_packages
-  # configure_git
-  # configure_lastpass
-  # configure_whatsyoursign
-  # install_launchagents
-  # copy_commands
-  # link_airport
-  # lower_startup_chime
-  # copy_dotfiles
-  # configure_dock
-  # os_customize
+  set_brew_default_apps
+  set_cask_default_apps
+  configure_zsh
+  install_atom_packages
+  configure_git
+  configure_lastpass
+  configure_whatsyoursign
+  install_launchagents
+  copy_commands
+  link_airport
+  lower_startup_chime
+  copy_dotfiles
+  configure_dock
+  os_customize
   #
-  # cleanup_brew
-  # cleanup_error_log
-  # move_manual_action_files
-  # killall caffeinate # computer can go back to sleep
-  # final_message
+  cleanup_brew
+  cleanup_error_log
+  move_manual_action_files
+  killall caffeinate computer can go back to sleep
+  final_message
 
   echo "I'm not on CI"
 fi
