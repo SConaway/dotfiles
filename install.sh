a1bdea8f2a92b9c92921d2e31911ac9ebd06eaa5
Steven Conaway <steven.conaway@icloud.com>
2018-02-17 09:03:49 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,7 @@
 #! /bin/bash
+
+curl --progress-bar --location 'https://github.com/sconaway/dotfiles/archive/master.zip' | ditto -xk - '/tmp' # download and extract script
+
 # run and log errors to file (but still show them when they happen)
 readonly error_log="${HOME}/Desktop/install_errors.log"
-bash ./main_install.sh "$1" 2> >(tee "${error_log}")
+bash /tmp/dotfiles-master/main_install.sh "$1" 2> >(tee "${error_log}")
