f62d0215bbcbf3066b4b0898413c5c0e607d2ed0
Steven Conaway <steven.conaway@icloud.com>
2018-07-20 17:32:41 -0700

    Changes

--- scripts/02_apps.sh
+++ scripts/02_apps.sh
@@ -34,7 +34,7 @@ install_cask_apps_part_1() {
 
   renew_sudo
 
-  readonly local cask_apps_1=('java' 'arduino' 'atom' 'caffeine' 'cheatsheet' 'coconutbattery' 'disk-inventory-x' 'docker' 'dropbox' 'eclipse-java' 'etcher' 'filezilla' 'flux' 'free-download-manager' 'firefox' 'fritzing' 'garmin-express' 'gfxcardstatus' 'gimp' 'gitup' 'google-chrome' 'iterm2' 'keka' 'kid3' 'knockknock' 'lastpass' 'lockdown' 'mediainfo' 'meld' 'minecraft' 'namechanger' 'openscad' 'osxfuse' 'razer-synapse' 'real-vnc' 'rocket' 'silicon-labs-vcp-driver' 'sketchup' 'steam')
+  readonly local cask_apps_1=('java' 'arduino' 'atom-beta' 'caffeine' 'cheatsheet' 'coconutbattery' 'disk-inventory-x' 'docker' 'dropbox' 'eclipse-java' 'etcher' 'filezilla' 'flux' 'free-download-manager' 'firefox' 'fritzing' 'garmin-express' 'gfxcardstatus' 'gimp' 'gitup' 'google-chrome' 'iterm2' 'keka' 'kid3' 'knockknock' 'lastpass' 'lockdown' 'mediainfo' 'meld' 'minecraft' 'namechanger' 'openscad' 'osxfuse' 'razer-synapse' 'real-vnc' 'rocket' 'silicon-labs-vcp-driver' 'sketchup' 'steam')
 
   for app in "${cask_apps_1[@]}"; do
     echo "Installing $app"
@@ -67,7 +67,7 @@ install_cask_apps_part_2() {
 
   renew_sudo
 
-  readonly local cask_apps_2=('java' 'taskexplorer' 'torbrowser' 'transmission' 'ultimaker-cura' 'virtualbox' 'vlc' 'caskroom/drivers/wch-ch34x-usb-serial-driver' 'wd-firmware-updater' 'vnc-viewer' 'vnc-server' 'xquartz' 'epubquicklook' 'qlcolorcode' 'qlimagesize' 'qlmarkdown' 'qlstephen' 'font-alegreya' 'font-alegreya-sans' 'font-fira-mono' 'font-fira-sans' 'font-input' 'font-merriweather' 'font-merriweather-sans' 'font-pt-mono' 'font-pt-sans' 'font-pt-serif' 'font-source-code-pro' 'font-source-sans-pro' 'font-source-serif-pro' 'font-aileron' 'font-bebas-neue' 'font-exo2' 'font-montserrat' 'font-lato' 'font-open-sans' 'font-open-sans-condensed' 'font-signika' 'font-abril-fatface' 'font-butler' 'font-gentium-book-basic' 'font-playfair-display' 'font-playfair-display-sc' 'font-bitter' 'font-kreon')
+  readonly local cask_apps_2=('java' 'taskexplorer' 'torbrowser' 'transmission' 'ultimaker-cura' 'virtualbox' 'vlc' 'caskroom/drivers/wch-ch34x-usb-serial-driver' 'wd-firmware-updater' 'vnc-viewer' 'vnc-server' 'xquartz' 'epubquicklook' 'qlcolorcode' 'qlimagesize' 'qlmarkdown' 'qlstephen' 'font-alegreya' 'font-alegreya-sans' 'font-fira-mono' 'font-fira-sans' 'font-input' 'font-merriweather' 'font-merriweather-sans' 'font-pt-mono' 'font-pt-sans' 'font-pt-serif' 'font-source-code-pro' 'font-source-sans-pro' 'font-source-serif-pro' 'font-aileron' 'font-bebas-neue' 'font-exo2' 'font-montserrat' 'font-lato' 'font-open-sans' 'font-open-sans-condensed' 'font-signika' 'font-abril-fatface' 'font-butler' 'font-gentium-book-basic' 'font-playfair-display' 'font-playfair-display-sc' 'font-bitter' 'font-kreon' 'font-powerline')
 
   for app in "${cask_apps_2[@]}"; do
     echo "Installing $app"
