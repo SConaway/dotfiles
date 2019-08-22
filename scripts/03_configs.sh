3ca3b5d500ec7d2afed253f6cc003de06b6bb87a
Steven Conaway <steven.conaway@icloud.com>
2019-08-21 18:04:42 -0700

    Allow iOS Simulator to run in Full Screen

--- scripts/03_configs.sh
+++ scripts/03_configs.sh
@@ -166,6 +166,9 @@ os_customize() {
 
   info 'Automatically quit printer app once the print jobs complete'
   defaults write com.apple.print.PrintingPrefs "Quit When Finished" -bool true
+  
+  info 'Allow iOS Simulator to run in Full Screen'
+  defaults write com.apple.iphonesimulator AllowFullscreenMode -bool true
 
   info 'Disable the “Are you sure you want to open this application?” dialog'
   defaults write com.apple.LaunchServices LSQuarantine -bool false
