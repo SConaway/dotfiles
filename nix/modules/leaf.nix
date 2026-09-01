63c849af2b610e87050c01f70d54d5ce25396f8e
Steven Conaway <steven.conaway@icloud.com>
2026-08-31 18:25:18 -0700

    nix: upgrade dependencies

--- nix/modules/leaf.nix
+++ nix/modules/leaf.nix
@@ -3,14 +3,14 @@
 let
   leaf = pkgs.rustPlatform.buildRustPackage {
     pname = "leaf";
-    version = "1.27.1";
+    version = "1.28.1";
     src = pkgs.fetchFromGitHub {
       owner = "RivoLink";
       repo = "leaf";
-      rev = "1.27.1";
-      hash = "sha256-JYKahP0dEineUKziR2uuxrVpZagtZr912m4Y4OBoloo=";
+      rev = "1.28.1";
+      hash = "sha256-xAO52Xhu2QOXzg/TJubTguJ7URddKnQekACnvytx5Qw=";
     };
-    cargoHash = "sha256-svfLThsZt3brVTlAloZizJuGlmnaiQIwjANg3oBCTIk=";
+    cargoHash = "sha256-Y+sOyHOSEjKW+NEpSjZgqJwXH3IOSFMBGM84oytRNsc=";
   };
 in
 {
