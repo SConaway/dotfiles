b06f97e75f723c1f0a8d9c3f591a5cf4e7071c13
Steven Conaway <steven.conaway@icloud.com>
2026-09-09 19:20:37 -0700

    nvim: render-markdown: set config before `vim.pack.add` runs

--- nvim/lua/plugins/render-markdown.lua
+++ nvim/lua/plugins/render-markdown.lua
@@ -1,12 +1,13 @@
 local util = require "config.util"
 
+-- put this here, rather than in `config()`, so that `vim.pack.add()` doesn't
+-- then run `.setup()` with the defaults
+vim.g.render_markdown_config = {
+  enabled = false,
+  completions = { lsp = { enabled = true } },
+}
+
 return {
   src = util.gh "MeanderingProgrammer/render-markdown.nvim", -- markdown!
   defer = true,
-  config = function()
-    require("render-markdown").setup {
-      enabled = false,
-      completions = { lsp = { enabled = true } },
-    }
-  end,
 }
